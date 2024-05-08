ccdd294d16c1473de35bbc128d142f8176fa38b3
JacobRestanio <jacobstanio@gmail.com>
2024-05-08 09:31:07 -0400

    Updates to make code easier to run and interpret.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import linprog_pulp as pulp
-import linprog_cplex as cplex
+# import linprog_pulp as solver
+import linprog_cplex as solver
 import divconq
 import maxflow
 import networkx as nx
@@ -19,13 +19,19 @@ def is_path_set(G, cps):
 
 
 def main():
-    num_different = 0
+    date = datetime.datetime.now()
+    with open("mcps_results.txt", "w") as file:
+        file.write(f"{date}\n")
+        file.write(f"test, size, divconq, maxflow, cplex\n")
+    with open("mcps_time.txt", "w") as file:
+        file.write(f"{date}\n")
+        file.write(f"test, size, divconq, maxflow, cplex\n")
     with open("mcps_memory.txt", "w") as file:
-        file.write(f"{datetime.datetime.now()}\n")
+        file.write(f"{date}\n")
         file.write(f"test, size, divconq, maxflow, cplex\n")
 
     for tree_size in [10, 100, 1000, 10000]:
-        for i in range(10):
+        for i in range(1000):
             print(f"test #{i+1}, size {tree_size}")
             tree = nx.random_tree(tree_size)
 
@@ -43,22 +49,19 @@ def main():
             current, maxflow_mem = tracemalloc.get_traced_memory()
             tracemalloc.stop()
 
-            # tracemalloc.start()
-            # st = time.time()
-            # pulp_mcps = pulp.find_mcps(tree)
-            # pulp_time = time.time() - st
-            # current, pulp_mem = tracemalloc.get_traced_memory()
-            # tracemalloc.stop()
-
             tracemalloc.start()
             st = time.time()
-            cplex_mcps = cplex.find_mcps(tree)
-            cplex_time = time.time() - st
-            current, cplex_mem = tracemalloc.get_traced_memory()
+            solver_mcps = solver.find_mcps(tree)
+            solver_time = time.time() - st
+            current, solver_mem = tracemalloc.get_traced_memory()
             tracemalloc.stop()
 
+            with open("mcps_results.txt", "a") as file:
+                file.write(f"{i+1}, {tree_size}, {len(divconq_mcps)}, {len(maxflow_mcps)}, {len(solver_mcps)}\n")
+            with open("mcps_time.txt", "a") as file:
+                file.write(f"{i+1}, {tree_size}, {divconq_time}, {maxflow_time}, {solver_time}\n")
             with open("mcps_memory.txt", "a") as file:
-                file.write(f"{i+1}, {tree_size}, {divconq_mem}, {maxflow_mem}, {cplex_mem}\n")
+                file.write(f"{i+1}, {tree_size}, {divconq_mem}, {maxflow_mem}, {solver_mem}\n")
 
 
 if __name__ == "__main__":
